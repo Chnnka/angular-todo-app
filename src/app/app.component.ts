21fbb132ea5d896b0abdc6a9b2beae72390ab45a
Channaka Gunawardhane <channakagunawardana@gmail.com>
2023-10-10 08:02:07 +0530

    20231010-0802

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WishItem } from 'src/shared/models/wishitem';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  items = [
+    new WishItem('To Learn Angular'),
+    new WishItem('Get Coffee', true),
+    new WishItem('Find missing item'),
+  ];
   title = 'wishlist';
 }
